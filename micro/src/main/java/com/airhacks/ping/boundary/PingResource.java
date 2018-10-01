8fadf622e71c8cb7f3cf626ccb596b7f4a7ec48c
Adam Bien <abien@adam-bien.com>
2018-10-01 08:56:48 +0200

    180_provider_side_timeouts

--- micro/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ micro/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -1,9 +1,13 @@
 package com.airhacks.ping.boundary;
 
+import java.util.concurrent.TimeUnit;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -17,8 +21,17 @@ public class PingResource {
     PingService service;
 
     @GET
-    public String ping() {
-        return this.service.message();
+    public void ping(@Suspended AsyncResponse response) {
+        response.setTimeout(500, TimeUnit.MILLISECONDS);
+        response.setTimeoutHandler(this::handleTimeout);
+        response.resume(this.service.message());
+    }
+
+    void handleTimeout(AsyncResponse asyncResponse) {
+        Response error = Response.status(503).
+                header("cause", "micro is overloaded, reponse takes longer than 500ms").
+                build();
+        asyncResponse.resume(error);
     }
 
 }
