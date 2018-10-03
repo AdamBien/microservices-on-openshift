6278f8a06c0ad7065eb250c38a6c02ddad7be481
Adam Bien <abien@adam-bien.com>
2018-10-03 11:15:24 +0200

    210_consumer_side_timeout

--- consumer/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ consumer/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -1,9 +1,13 @@
 package com.airhacks.ping.boundary;
 
+import java.util.concurrent.TimeUnit;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -18,8 +22,15 @@ public class PingResource {
 
 
     @GET
-    public String ping() {
-        return "Enjoy Java EE 8, calling micro -> " + this.microClient.ping();
+    public void ping(@Suspended AsyncResponse response) {
+        response.setTimeout(2, TimeUnit.SECONDS);
+        response.setTimeoutHandler(this::handleTimeout);
+
+        response.resume("Enjoy Java EE 8, calling micro -> " + this.microClient.ping());
     }
 
+    void handleTimeout(AsyncResponse asyncResponse) {
+        Response errorResponse = Response.status(503).header("cause", "consumer took too long").build();
+        asyncResponse.resume(errorResponse);
+    }
 }
